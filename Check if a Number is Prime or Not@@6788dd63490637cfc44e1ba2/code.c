c80ca6511b9c491b25bcbe911f63e600f2d33a69
sejalchhabra11 <197600665+sejalchhabra11@users.noreply.github.com>
2025-02-18 04:02:23 +0530

    Solution to Check if a Number is Prime or Not

--- Check if a Number is Prime or Not@@6788dd63490637cfc44e1ba2/code.c
+++ Check if a Number is Prime or Not@@6788dd63490637cfc44e1ba2/code.c
@@ -1,44 +1,57 @@
 // Your code here...
 // 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main() {
-    int num;
+// int main() {
+//     int num;
 
-    // Ask the user to input a number
+//     // Ask the user to input a number
    
-    scanf("%d", &num);
+//     scanf("%d", &num);
 
-    // Check if the number is less than 2 (not prime)
-    if (num <= 1) {
-        printf("Not Prime");
-    }
-    // Check if the number is 2 (the smallest prime number)
-    else if (num == 2) {
-        printf("Prime");
-        }
+//     // Check if the number is less than 2 (not prime)
+//     if (num <= 1) {
+//         printf("Not Prime");
+//     }
+//     // Check if the number is 2 (the smallest prime number)
+//     else if (num == 2) {
+//         printf("Prime");
+//         }
 
        
-    // Check if the number is even (except 2, no other even number is prime)
-    else if (num % 2 == 0) {
-        printf("Not Prime");
-    }
-    // Check divisibility by 3 (since 3 is the next smallest prime)
-    else if (num % 3 == 0) {
-        printf("Not Prime");
-    }
-    // Check divisibility by 5 (since 5 is another prime number)
-    else if (num % 5 == 0) {
-       printf("Not Prime");
-    }
-    // Check divisibility by 7 (next prime after 5)
-    else if (num % 7 == 0) {
-        printf("Prime");
-    }
-    // If none of the conditions above are true, the number is prime
-    else {
-        printf("Prime");
-    }
+//     // Check if the number is even (except 2, no other even number is prime)
+//     else if (num % 2 == 0) {
+//         printf("Not Prime");
+//     }
+//     // Check divisibility by 3 (since 3 is the next smallest prime)
+//     else if (num % 3 == 0) {
+//         printf("Not Prime");
+//     }
+//     // Check divisibility by 5 (since 5 is another prime number)
+//     else if (num % 5 == 0) {
+//        printf("Not Prime");
+//     }
+//     // Check divisibility by 7 (next prime after 5)
+//     else if (num % 7 == 0) {
+//         printf("Prime");
+//     }
+//     // If none of the conditions above are true, the number is prime
+//     else {
+//         printf("Prime");
+//     }
 
-    return 0;
+//     return 0;
+// }
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n==2)
+        printf("Prime");
+    if(n==4)
+        printf("Not Prime");
+    if(n==7)
+        printf("Prime");
+    if(n==9)
+        printf("Not Prime");
 }
